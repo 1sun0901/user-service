ac5398309528b2dc85ec05511b237dddbf92b0dc
1sun0901 <emuce@naver.com>
2022-07-19 22:47:52 +0900

    Micrometer, prometheus 및 grafana 적용

--- src/main/java/com/example/userservice/controller/UserController.java
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -8,6 +8,7 @@ import com.example.userservice.vo.Greeting;
 import com.example.userservice.vo.RequestUser;
 import com.example.userservice.vo.ResponseOrder;
 import com.example.userservice.vo.ResponseUser;
+import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
@@ -31,6 +32,7 @@ public class UserController {
 
 
     @GetMapping("/health_check")
+    @Timed(value="users.status", longTask = true)
     public String status() {
        return String.format("It is working in User Service"
                + ", port(local.server.port) = " + env.getProperty("local.server.port")
@@ -40,6 +42,7 @@ public class UserController {
     }
 
     @GetMapping("/welcome")
+    @Timed(value="users.welcome", longTask = true)
     public String welcome() {
         return env.getProperty("greeting.message");
     }
